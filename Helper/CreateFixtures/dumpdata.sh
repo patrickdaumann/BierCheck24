5d5cc5cff9f32b6a7ad91c3d9d01b4a8e24a7d2f
Patrick Daumann <59425260+patrickdaumann@users.noreply.github.com>
2023-06-30 10:41:44 +0200

    changed dump helper

--- Helper/CreateFixtures/dumpdata.sh
+++ Helper/CreateFixtures/dumpdata.sh
@@ -1,7 +1,34 @@
 #!/bin/zsh
 
 directory="dumps"
-filename=$(date +"%Y%m%d")_alldata.json
-fullpath="${directory}/${filename}"
+fileextension=".json"
+date="$(date +"%Y%m%d")"
 
+
+filename="alldata"
+fullpath="${directory}/${date}_${filename}${fileextension}"
+echo $fullpath
 python3 manage.py dumpdata Rating > $fullpath
+
+
+filename="beers"
+fullpath="${directory}/${date}_${filename}${fileextension}"
+echo $fullpath
+python3 manage.py dumpdata Rating.beer > $fullpath
+
+filename="breweries"
+fullpath="${directory}/${date}_${filename}${fileextension}"
+echo $fullpath
+python3 manage.py dumpdata Rating.brewery > $fullpath
+
+filename="beertypes"
+fullpath="${directory}/${date}_${filename}${fileextension}"
+echo $fullpath
+python3 manage.py dumpdata Rating.beertype > $fullpath
+
+filename="recommendations"
+fullpath="${directory}/${date}_${filename}${fileextension}"
+echo $fullpath
+python3 manage.py dumpdata Rating.recommendation > $fullpath
+
+
