6d8d53861abf2e699f0050ae7e22b3ee36888274
Patrick Daumann <59425260+patrickdaumann@users.noreply.github.com>
2023-07-13 20:10:46 +0200

    -

--- Helper/Setup/setupdb.sh
+++ Helper/Setup/setupdb.sh
@@ -6,11 +6,13 @@ python3 manage.py migrate --run-syncdb
 python3 manage.py loaddata Users.json --app Rating 
 python3 manage.py loaddata 20230713_alldata.json --app Rating
 
-python3 manage.py loaddata Beertypes.json --app Rating
-python3 manage.py loaddata Breweries.json --app Rating
-python3 manage.py loaddata Beers.json --app Rating
-python3 manage.py loaddata Recommendations.json --app Rating
-python3 manage.py loaddata Ratings_gen.json --app Rating
+
+## Nicht mehr notwendig:
+#python3 manage.py loaddata Beertypes.json --app Rating
+#python3 manage.py loaddata Breweries.json --app Rating
+#python3 manage.py loaddata Beers.json --app Rating
+#python3 manage.py loaddata Recommendations.json --app Rating
+#python3 manage.py loaddata Ratings_gen.json --app Rating
 
 
 echo "<<python3 manage.py createsuperuser>> nicht vergessen!"
